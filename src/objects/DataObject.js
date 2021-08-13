8345fa19b6f2244076f20e6dd6c19f4f0ac56b61
Akasha <Akasha.Rojee@gmail.com>
2021-08-14 01:42:50 +0400

    Fix linter errors

--- src/objects/DataObject.js
+++ src/objects/DataObject.js
@@ -1,6 +1,5 @@
-/* eslint-disable no-unused-vars */
 export default class DataObject {
-  constructor(total, next, operation) {
+  constructor() {
     this.total = '0';
     this.next = null;
     this.operation = null;
