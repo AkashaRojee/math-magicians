3941ed437b67d2bdba26ac0574f6037d92764ee1
Akasha <Akasha.Rojee@gmail.com>
2021-08-18 21:11:20 +0400

    Add TextBody component with paragraphs

---
+++ src/components/TextBody.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+
+const TextBody = (props) => {
+  
+  return (
+    <div>
+      {props.paragraphs.map((paragraph, index) => (
+        <p key={index}>{paragraph}</p>
+      ))}
+    </div>
+  );
+}
+
+export default TextBody;
