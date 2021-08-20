e13071a6b8a562e4fb540735922cdbd721aaea3d
Akasha <Akasha.Rojee@gmail.com>
2021-08-20 08:07:30 +0400

    Test for clicking on Calculator menu link

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,9 +1,23 @@
 import renderer from 'react-test-renderer';
 import App from '../App';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
 
 describe('App', () => {
   it('renders correctly', () => {
     const tree = renderer.create(<App />);
     expect(tree).toMatchSnapshot();
+  });
+});
+
+test('Clicking on Calculator link in navigation bar displays calculator in App', () => {
+  render(<App />);
+  const calculatorLink = screen.getByText('Calculator');
+
+  userEvent.click(calculatorLink);
+  const calculatorButtons = screen.getAllByRole('button');
+  
+  calculatorButtons.forEach((calculatorButton) => {
+    expect(calculatorButton).toBeVisible();
   });
 });
